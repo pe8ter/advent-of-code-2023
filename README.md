64e67fe8e757a850337fc2e5bc58594167eed672
Peter Safranek <pe8ter@mac.com>
2023-12-07 00:01:18 -0800

    Complete Day 2, Part Two

--- README.md
+++ README.md
@@ -9,4 +9,5 @@ node ./src/day-01-part-1/main.mjs --input=./src/day-01-part-1/input.txt
 node ./src/day-01-part-2/main.mjs --input=./src/day-01-part-2/input.txt
 
 node ./src/day-02-part-1/main.mjs --input=./src/day-02-part-1/input.txt --red=12 --green=13 --blue=14
+node ./src/day-02-part-2/main.mjs --input=./src/day-02-part-2/input.txt
 ```
---
+++ src/day-02-part-2/main.mjs
@@ -0,0 +1,108 @@
+import { readFile } from 'node:fs/promises';
+import { EOL } from 'node:os';
+
+import { getArgs } from '../utils/args.mjs';
+
+/**
+ * @typedef {{red: number, green: number, blue: number}} CubeSet
+ */
+
+/**
+ * @typedef {{index: number, sets: Array<CubeSet>}} Game
+ */
+
+/**
+ * Extract the results for a "game" for an input line. A game includes its index and a list of collection of colors.
+ * Each color represents the number of each cube color drawn in a set. Each set is separated by semi-colons. Each color
+ * is separated by commas.
+ *
+ * Example: "Game 42: 5 red, 2 green, 1 blue; 20 red" becomes:
+ *     { index: 42, sets: [{ red: 5, green: 2, blue: 1 }, { red: 20, green: 0, blue: 0 }] }
+ *
+ * Notice how we fill missing colors in with zeros.
+ * @param {string} line One line of text from the input file.
+ * @returns {Game} Data structure that represent a game.
+ */
+
+function convertInputLineToGame(line) {
+    const indexRegex = /(?<index>[\d]+)/;
+
+    const indexAndSetsText = line.split(':');
+    const index = Number.parseInt(indexRegex.exec(indexAndSetsText[0]));
+
+    const sets = [];
+
+    for (const setText of indexAndSetsText[1].split(';')) {
+        let red = 0;
+        let green = 0;
+        let blue = 0;
+        for (const colorResultText of setText.split(',')) {
+            const countAndColor = colorResultText.trim().split(' ');
+            const count = Number.parseInt(countAndColor[0]);
+            const color = countAndColor[1].toLowerCase();
+            switch (color) {
+                case 'red':
+                    red = count;
+                    break;
+                case 'green':
+                    green = count;
+                    break;
+                case 'blue':
+                    blue = count;
+                    break;
+                default:
+                    throw new Error(`Did not recognize color "${color}"`);
+            }
+        }
+        sets.push({ red, green, blue });
+    }
+
+    const result = { index, sets };
+
+    return result;
+}
+
+/**
+ * Find the minimum number of each color of cubes that a game must have.
+ * @param {Game} game
+ * @returns {CubeSet}
+ */
+
+function findMinimumNumberOfCubes(game) {
+    const red = Math.max(...game.sets.map(set => set.red));
+    const green = Math.max(...game.sets.map(set => set.green));
+    const blue = Math.max(...game.sets.map(set => set.blue));
+
+    const result = { red, green, blue };
+
+    return result;
+}
+
+/**
+ * Main entry point.
+ */
+
+async function main() {
+    const args = getArgs({
+        input: {
+            type: 'string',
+        },
+    });
+
+    const input = args.input;
+
+    const inputText = await readFile(input, { encoding: 'utf-8' });
+
+    const total = inputText
+        .split(EOL)
+        .filter(line => line)
+        .map((line) => convertInputLineToGame(line))
+        .map((game) => findMinimumNumberOfCubes(game))
+        .reduce((accum, min) => accum + min.red * min.green * min.blue, 0);
+
+    console.log(`Answer: ${total}`);
+}
+
+main()
+    .then(() => console.log('Script completed successfully.'))
+    .catch((err) => console.error('Script failed with error', err));
